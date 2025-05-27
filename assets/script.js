0eebfd71ac9dada99b24eae8c8124ffe0344b352
reddii7 <104622120+reddii7@users.noreply.github.com>
2025-05-27 11:18:06 +0100

    v7

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 // Register the service worker
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('/assets/service-worker.js')
         .then(registration => console.log('Service Worker registered with scope:', registration.scope))
         .catch(function (error) {
             console.error('Service Worker registration failed:', error);
